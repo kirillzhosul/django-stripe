2318db74ffe1cf44d11b9fef76089b69bcc3f0e6
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-09-18 21:35:00 +0400

    Handle URL prefix for static files

--- src/stripe_app/settings.py
+++ src/stripe_app/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = f"{URL_PREFIX}/static/" if URL_PREFIX else "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
